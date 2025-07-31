1b0765b94c960c4235e0b08a88fae1b70749ef7f
keell <kellylecardinal@gmail.com>
2025-07-31 15:42:08 +0200

    prep-data

--- src/prep_data.py
+++ src/prep_data.py
@@ -1,7 +1,7 @@
 import chess.pgn
 import pandas as pd
 import numpy as np
-
+ 
 #load le pgn et retourne une liste de parties
 def load_games(pgn_path, max_games=10):
     games = []
@@ -13,10 +13,10 @@ def load_games(pgn_path, max_games=10):
                 break
             games.append(game)
     return games
-
+ 
 #convertit les parties en un fichier csv
 #avec les colonnes : game_id, fen, move_played, turn, result
-
+ 
 def pgn_to_csv(pgn_path, max_games=10, output_csv="chess_positions.csv"):
     data = []
     with open(pgn_path) as f:
@@ -25,7 +25,7 @@ def pgn_to_csv(pgn_path, max_games=10, output_csv="chess_positions.csv"):
             game = chess.pgn.read_game(f)
             if game is None or game_id >= max_games:
                 break
-
+ 
             result = game.headers["Result"]
             if result == "1-0":
                 label = 1
@@ -33,14 +33,14 @@ def pgn_to_csv(pgn_path, max_games=10, output_csv="chess_positions.csv"):
                 label = 0
             else:
                 label = 0.5
-
+ 
             board = game.board()
             for move in game.mainline_moves():
                 fen = board.fen()
                 turn = fen.split()[1]  # 'w' or 'b'
                 board.push(move)
                 uci_move = move.uci()
-
+ 
                 data.append({
                     "game_id": game_id,
                     "fen": fen,
@@ -48,24 +48,24 @@ def pgn_to_csv(pgn_path, max_games=10, output_csv="chess_positions.csv"):
                     "turn": turn,
                     "result": label
                 })
-
+ 
             game_id += 1
-
+ 
     df = pd.DataFrame(data)
     df.to_csv(output_csv, index=False)
-    print(f"✅ Fichier CSV généré avec {len(df)} positions.")
-
-
+    print(f"Fichier CSV généré avec {len(df)} positions.")
+ 
+ 
 PIECE_TO_INDEX = {
     'P': 0, 'N': 1, 'B': 2, 'R': 3, 'Q': 4, 'K': 5,
     'p': 6, 'n': 7, 'b': 8, 'r': 9, 'q': 10, 'k': 11
 }
-
+ 
 def fen_to_tensor(fen):
     board_tensor = np.zeros((8, 8, 12), dtype=np.float32)
     board_fen = fen.split()[0]
     rows = board_fen.split('/')
-
+ 
     for i, row in enumerate(rows):
         col = 0
         for char in row:
