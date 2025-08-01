ad4d084e838a9b70a954823dfa95593129bc87be
keell <kellylecardinal@gmail.com>
2025-08-01 10:09:19 +0200

    modif fonction eval

--- src/model.py
+++ src/model.py
@@ -7,7 +7,13 @@ def create_model():
     # en entrée on met 768 features (12 pièces x 8 rangées x 8 colonnes)
     model = models.Sequential([
         layers.Dense(512, activation='relu', input_shape=(768,)),  # Couche 1 + couche d'entrée cachée
-        layers.Dense(256, activation='relu'),                     # Couche 2
-        layers.Dense(1)                                           # Couche de sortie
+        # layers.PReLU(),  
+        layers.Dense(256, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),  # Couche 2
+        layers.Dropout(0.3),
+        # layers.PReLU(),  
+        # layers.Dense(128, activation='relu'),     # Couche 3
+        # layers.Dense(64,activation='relu'),      # Couche 4
+        # layers.PReLU(),  
+        layers.Dense(1)        # Couche de sortie
     ])
     return model
